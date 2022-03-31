b2962da6289f18d7729dc0cf6313e503253a3756
JoannaAgo <Besianaioannaago@gmail.com>
2022-03-31 20:42:42 +0300

    code

--- code/app/src/main/java/com/example/project/Matchfound.java
+++ code/app/src/main/java/com/example/project/Matchfound.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Matchfound extends AppCompatActivity {
 
@@ -13,10 +14,23 @@ public class Matchfound extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matchfound);
 
+        TextView button = findViewById(R.id.textView3);
 
+        button.setOnClickListener(view -> {
 
+            Intent intent = new Intent( this, Payment.class);
 
+            startActivity(intent);
+        });
+        TextView newbutton = findViewById(R.id.textView8);
+
+        newbutton.setOnClickListener(view -> {
+
+            Intent intent = new Intent( this, Payment.class);
+
+            startActivity(intent);
+        });
 
 
     }
-}
+};
