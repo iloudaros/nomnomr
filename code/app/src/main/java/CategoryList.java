23f28f068acfe32603915b476a759ff28c34fac0
Christina Kt <xriskt@gmail.com>
2022-06-06 20:39:17 +0300

    Added CategoryList Class Code

---
+++ code/app/src/main/java/CategoryList.java
@@ -0,0 +1,11 @@
+package com.example.project;
+
+//import java.util.ArrayList;
+
+public class CategoryList {
+    ArrayList<String> list = new ArrayList<String>();
+
+
+public something fetchCategories() {
+
+}
