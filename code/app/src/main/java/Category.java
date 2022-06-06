09c38f543877eb4bbe4a35e43f121a01381fc6dd
Christina Kt <xriskt@gmail.com>
2022-06-06 20:38:56 +0300

    Added Category Class Code

---
+++ code/app/src/main/java/Category.java
@@ -0,0 +1,31 @@
+package com.example.project;
+
+public class Category {
+    private enum title{
+      Asian,
+      Mexican,
+      Indian,
+      ...
+    }
+
+    private String description;
+
+    public title getTitle() {
+      return title;
+    }
+
+    public String getDescription() {
+      return description;
+    }
+
+    public void setTitle(title title) {
+      this.title = title;
+    }
+
+    public void setDescription(String description) {
+      this.description = description;
+    }
+
+    public void addCategories(Category) {
+      return 1;
+    }
