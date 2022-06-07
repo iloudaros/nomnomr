ea288195968907f17fba613ee43ee6db22351063
IoannaAgo <75138406+JoannaAgo@users.noreply.github.com>
2022-06-07 18:30:56 +0300

    Class Map added

--- code/app/src/main/java/Map.java
+++ code/app/src/main/java/Map.java
@@ -1,55 +1,50 @@
-import com.google.android.gms.common.api.GoogleApiClient;  
-import com.google.android.gms.maps.GoogleMap; 
-import android.location.Location;  
-import com.google.android.gms.common.ConnectionResult;   
-import com.google.android.gms.location.LocationRequest;  
-import com.google.android.gms.maps.model.Marker;  
-import com.google.android.gms.maps.model.MarkerOptions;  
-import com.google.android.gms.location.LocationServices;  
+//import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.maps.GoogleMap;
+//import android.location.Location;
+//import android.view.MenuItem;
+//
+//import com.google.android.gms.common.ConnectionResult;
+//import com.google.android.gms.location.LocationRequest;
+//import com.google.android.gms.maps.model.Marker;
+//import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
 
 public class Map {
-    private GoogleMap mMap;  
-    private Location mLastLocation;  
-    private Marker mCurrLocationMarker;  
-    private GoogleApiClient mGoogleApiClient;  
-    private LocationRequest mLocationRequest; 
+//    private GoogleMap mMap;
+//    private Location mLastLocation;
+//    private Marker mCurrLocationMarker;
+//    private GoogleApiClient mGoogleApiClient;
+//    private LocationRequest mLocationRequest;
     
+     private ArrayList<Address> addresses = new ArrayList<>();
 
-
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap; 
+    public ArrayList<Address> getAddresses() {
+        return addresses;
     }
 
-    protected synchronized void buildGoogleApiClient() {  
-        mGoogleApiClient = new GoogleApiClient.Builder(this)  
-                .addConnectionCallbacks(this)  
-                .addOnConnectionFailedListener(this)  
-                .addApi(LocationServices.API).build();  
-        mGoogleApiClient.connect();  
-    }     
-
-
-    public void onConnected(Bundle bundle) {  
-  
-        mLocationRequest = new LocationRequest();  
+    public void setAddresses(ArrayList<Address> addresses) {
+        this.addresses = addresses;
     }
 
-
-    public void onConnectionSuspended(int i) {  
-  
-    }  
-  
-    public void onLocationChanged(Location location) {  
-  
-        mLastLocation = location;  
-        if (mCurrLocationMarker != null) {  
-            mCurrLocationMarker.remove();  
-        }  
-    }
-
-    
-    public void onConnectionFailed(ConnectionResult connectionResult) {  
-  
-    }  
+    //    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//    }
+//    protected synchronized void buildGoogleApiClient() {
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API).build();
+//        mGoogleApiClient.connect();   }
+//    public void onConnected(Bundle bundle) {
+//        mLocationRequest = new LocationRequest();   }
+//    public void onConnectionSuspended(int i) {}
+//    public void onLocationChanged(Location location) {
+//        mLastLocation = location;
+//        if (mCurrLocationMarker != null) {
+//            mCurrLocationMarker.remove();
+//        } }
+//    public void onConnectionFailed(ConnectionResult connectionResult) {}
     
 }
