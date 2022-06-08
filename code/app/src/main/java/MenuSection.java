bfb14072f9eea076beb4be5aecb205be8734ec72
Athanasia Zekyria <athanasiazekyria@gmail.com>
2022-06-08 15:15:25 +0300

    new java classes

---
+++ code/app/src/main/java/MenuSection.java
@@ -0,0 +1,9 @@
+public class MenuSection {
+    private String title;
+    ArrayList<MenuItem> items = new ArrayList<MenuItem>();
+
+
+    public void addItem(MenuItem){
+        return 1;
+    }
+}
